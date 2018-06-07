991ac5638c85803216cf9f69dd609ee0f170db2e
Chukwuemeka Ihedoro <caihedoro@gmail.com>
2018-06-07 23:15:40 +0100

    minor fix

--- Model/Config/Source/OrderStatus.php
+++ Model/Config/Source/OrderStatus.php
@@ -1,23 +1,4 @@
 <?php
-/**
- * Payscrow Premium Escrow Service enables your store to easily connect with the Payscrow payment Service in Nigeria
- * Copyright (C) 2017  
- * 
- * This file is part of Payscrow/PaymentGateway.
- * 
- * Payscrow/PaymentGateway is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 
 namespace Payscrow\PaymentGateway\Model\Config\Source;
 
@@ -26,11 +7,11 @@ class OrderStatus implements \Magento\Framework\Option\ArrayInterface
 
     public function toOptionArray()
     {
-        return [['value' => 'Pending', 'label' => __('Pending')],['value' => 'Complete', 'label' => __('Complete')],['value' => 'Processing', 'label' => __('Processing')],['value' => 'Cancelled', 'label' => __('Cancelled')]];
+        return [['value' => 'Pending', 'label' => __('Pending')], ['value' => 'Complete', 'label' => __('Complete')], ['value' => 'Processing', 'label' => __('Processing')], ['value' => 'Cancelled', 'label' => __('Cancelled')]];
     }
 
     public function toArray()
     {
-        return ['Pending' => __('Pending'),'Complete' => __('Complete'),'Processing' => __('Processing'),'Cancelled' => __('Cancelled')];
+        return ['Pending' => __('Pending'), 'Complete' => __('Complete'), 'Processing' => __('Processing'), 'Cancelled' => __('Cancelled')];
     }
 }
