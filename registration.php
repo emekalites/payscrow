ea0a9d60cabf61cc06ea09c8c79c61b89500e582
Chukwuemeka Ihedoro <caihedoro@gmail.com>
2018-06-07 20:55:35 +0100

    minor fixes

--- registration.php
+++ registration.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: emnity
- * Date: 5/21/18
- * Time: 5:08 AM
- */
-
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Payscrow_PaymentGateway',
